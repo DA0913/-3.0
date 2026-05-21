541dead3d553f7cc3ace3e5840a792063c90c8fb
DA0913 <1085442378@qq.com>
2026-05-21 15:44:20 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'events': 'events',
+    },
+  },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
+});
